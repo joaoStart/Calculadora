8f5886b5438cc22056e7b83ac5e53fdf94e1d865
joão victor <jvictor491@gmail.com>
2020-08-22 14:40:07 -0300

    primeiros passos no JS

---
+++ main.js
@@ -0,0 +1,16 @@
+function onclickOperador () {
+    var operador = document.getElementsByClassName('operador')[1].value;
+    if (operador === '+') {
+        console.log('soma')
+    }
+}
+
+function onClickButtonOp () {
+   var valueN1 = parseInt(document.getElementById('campoValor1').value);
+   
+   var valueN2 =parseInt( document.getElementById('campoValor2').value);
+
+   if(operador === '+') {
+       console.log(soma)
+   }
+}
