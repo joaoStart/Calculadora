3c9bce6259050425d41e9256ec949527e996a288
joão victor <jvictor491@gmail.com>
2020-08-24 21:35:08 -0300

    versão com Botões

--- main.js
+++ main.js
@@ -1,16 +1,28 @@
-function onclickOperador () {
-    var operador = document.getElementsByClassName('operador')[1].value;
-    if (operador === '+') {
-        console.log('soma')
-    }else if (operador === '-') {
-        
-    }
+var operador;
+
+function onclickOperador (valueOperador) {
+    this.operador = valueOperador;
 }
 
 function onClickButtonOp () {
    var valueN1 = parseInt(document.getElementById('campoValor1').value);
-   
    var valueN2 =parseInt( document.getElementById('campoValor2').value);
 
+   switch (operador) {
+       case '+' :
+           alert(valueN1 + valueN2);
+        break;
+        case '-':
+            alert(valueN1 - valueN2);
+        break;
+        case '*':
+            alert(valueN1 * valueN2);
+        break;
+        case '/':
+            alert(valueN1 / valueN2);
+        break;
+        default:
+            alert('Nenhum operador foi selacionado.')
+   }
    
 }
