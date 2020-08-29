32b04e77fe73e4836fb3587c72e3a47c60e4e1c4
joão victor <jvictor491@gmail.com>
2020-08-29 15:43:27 -0300

    aplicação do css e botões

--- main.js
+++ main.js
@@ -1,28 +1,36 @@
 var operador;
 
-function onclickOperador (valueOperador) {
-    this.operador = valueOperador;
+function onclickOperador (valueOper) {//exibir os operadores na tela
+    document.getElementById('visor').value += valueOper;
+    this.operador = valueOper;
+}
+function onclickNumber(num) {//exibir os numeros na tela
+    document.getElementById('visor').value += num;
+
+}
+function clearAll() {//Limpar o conteudo da tela 
+    document.getElementById('visor').value = "";
 }
 
-function onClickButtonOp () {
-   var valueN1 = parseInt(document.getElementById('campoValor1').value);
-   var valueN2 =parseInt( document.getElementById('campoValor2').value);
+// function onClickButtonOp () {
+//    var valueN1 = parseInt(document.getElementById('campoValor1').value);
+//    var valueN2 =parseInt( document.getElementById('campoValor2').value);
 
-   switch (operador) {
-       case '+' :
-           alert(valueN1 + valueN2);
-        break;
-        case '-':
-            alert(valueN1 - valueN2);
-        break;
-        case '*':
-            alert(valueN1 * valueN2);
-        break;
-        case '/':
-            alert(valueN1 / valueN2);
-        break;
-        default:
-            alert('Nenhum operador foi selacionado.')
-   }
+//    switch (operador) {
+//        case '+' :
+//            alert(valueN1 + valueN2);
+//         break;
+//         case '-':
+//             alert(valueN1 - valueN2);
+//         break;
+//         case '*':
+//             alert(valueN1 * valueN2);
+//         break;
+//         case '/':
+//             alert(valueN1 / valueN2);
+//         break;
+//         default:
+//             alert('Nenhum operador foi selacionado.')
+//    }
    
-}
+// }
