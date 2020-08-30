827dcd26df173aa38ff8fc779ad3cf2bdc5e1d02
joão victor <jvictor491@gmail.com>
2020-08-29 21:54:51 -0300

    Versão totalmente funcional da calculadora

--- main.js
+++ main.js
@@ -1,36 +1,18 @@
-var operador;
-
 function onclickOperador (valueOper) {//exibir os operadores na tela
     document.getElementById('visor').value += valueOper;
-    this.operador = valueOper;
+}
+function onChangeInput () {
+    document.getElementById('visor').style.color = 'black';
 }
 function onclickNumber(num) {//exibir os numeros na tela
     document.getElementById('visor').value += num;
-
 }
 function clearAll() {//Limpar o conteudo da tela 
+    document.getElementById('visor').style.color = 'black';
     document.getElementById('visor').value = "";
 }
-
-// function onClickButtonOp () {
-//    var valueN1 = parseInt(document.getElementById('campoValor1').value);
-//    var valueN2 =parseInt( document.getElementById('campoValor2').value);
-
-//    switch (operador) {
-//        case '+' :
-//            alert(valueN1 + valueN2);
-//         break;
-//         case '-':
-//             alert(valueN1 - valueN2);
-//         break;
-//         case '*':
-//             alert(valueN1 * valueN2);
-//         break;
-//         case '/':
-//             alert(valueN1 / valueN2);
-//         break;
-//         default:
-//             alert('Nenhum operador foi selacionado.')
-//    }
-   
-// }
+function onClickButtonOp () {
+   var value = document.getElementById('visor').value;
+   document.getElementById('visor').value = eval(value).toString();
+   document.getElementById('visor').style.color = 'blue'
+}
